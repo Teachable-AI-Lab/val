2db651c31730c2b25eda8eff3282836af57d1039
Christopher MacLellan <cmaclell@gatech.edu>
2024-05-20 18:13:08 -0400

    working connection

---
+++ val/user_interfaces/launch_socket_io_server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('chat.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('message')
+def handle_message(data):
+    print(f'Received message: { data }')
+
+@socketio.on('request_user_task')
+def handle_request_user_Task(data):
+    print(f'Received request_user_task: { data }')
+
+if __name__ == '__main__':
+    socketio.run(app, port=4000)
