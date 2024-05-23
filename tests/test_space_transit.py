07c0dd33fc4df3d1f29237a8f483bc0ab09ff323
Christopher MacLellan <cmaclell@gatech.edu>
2024-05-23 16:03:09 -0400

    workig examples

--- tests/test_space_transit.py
+++ tests/test_space_transit.py
@@ -1,5 +1,6 @@
 from val.agent import ValAgent
 from val.user_interfaces.console_interface import ConsoleUserInterface
+from val.user_interfaces.web_interface import WebUserInterface
 from val.env_interfaces.space_transit.space_transit_env import SpaceTransitEnv 
 from val.htn_interfaces.basic_htn_interface import BasicHtnInterface
 from val.utils import get_openai_key
@@ -9,11 +10,12 @@ if __name__ == "__main__":
     openai_key = get_openai_key()
 
     env = SpaceTransitEnv()
-    # htn_interface = PyHtnInterface
     user_interface = ConsoleUserInterface
+    # user_interface = WebInterface
     htn_interface = BasicHtnInterface
+    # htn_interface = PyHtnInterface
 
 
-    agent = ValAgent(env, ConsoleUserInterface, BasicHtnInterface, openai_key)
+    agent = ValAgent(env, user_interface, htn_interface, openai_key)
     agent.start()
 
--- tests/test_tictactoe_basic.py
+++ tests/test_tictactoe_basic.py
@@ -1,11 +1,7 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import subprocess
-
 from val.agent import ValAgent
 from val.user_interfaces.console_interface import ConsoleUserInterface
 from val.user_interfaces.web_interface import WebInterface
+from val.user_interfaces.console_interface import ConsoleUserInterface
 from val.env_interfaces.tictactoe.tictactoe_env import TicTacToeEnv 
 from val.htn_interfaces.basic_htn_interface import BasicHtnInterface
 from val.htn_interfaces.py_htn_interface import PyHtnInterface
@@ -16,15 +12,17 @@ if __name__ == "__main__":
     openai_key = get_openai_key()
 
     env = TicTacToeEnv()
-    # htn_interface = PyHtnInterface
-    user_interface = WebInterface
+    # user_interface = WebInterface
+    user_interface = ConsoleUserInterface
     htn_interface = BasicHtnInterface
     # htn_interface = PyHtnInterface
-    if user_interface == WebInterface:
-        relative_path = os.path.join('..', 'val', 'val', 'user_interfaces', 'launch_server.py')
-        target_file = os.path.abspath(relative_path)
-        print(f"Target file path: {target_file}")
-        process = subprocess.Popen(['python', target_file])      
+
+    # if user_interface == WebInterface:
+    #     relative_path = os.path.join('..', 'val', 'val', 'user_interfaces', 'launch_server.py')
+    #     target_file = os.path.abspath(relative_path)
+    #     print(f"Target file path: {target_file}")
+    #     process = subprocess.Popen(['python', target_file])      
+
     agent = ValAgent(env, user_interface, htn_interface, openai_key)
     agent.start()
 
