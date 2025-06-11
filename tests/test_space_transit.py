64867b885c273cd89a1250c496d83e3dc710bbc5
Qiao Zhang <qiaozhd2@gmail.com>
2025-06-11 16:39:09 -0400

    fixed typo

--- tests/test_space_transit.py
+++ tests/test_space_transit.py
@@ -2,7 +2,7 @@ from val.agent import ValAgent
 from val.htn_interfaces.py_htn_interface import PyHtnInterface
 from val.user_interfaces.console_interface import ConsoleUserInterface
 from val.user_interfaces.web_interface import WebInterface
-from val.user_interfaces.speech_to_text_interface import SpeechToTextInterface
+from val.user_interfaces.speech_to_text_interface import SpeechInterface
 from val.env_interfaces.space_transit.space_transit_env import SpaceTransitEnv 
 from val.htn_interfaces.basic_htn_interface import BasicHtnInterface
 from val.utils import get_openai_key
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     env = SpaceTransitEnv()
     # user_interface = ConsoleUserInterface
     # user_interface = WebInterface
-    user_interface = SpeechToTextInterface
+    user_interface = SpeechInterface
     # htn_interface = BasicHtnInterface
     htn_interface = PyHtnInterface
 
